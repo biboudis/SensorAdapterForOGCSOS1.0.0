4fa42134acee73291581777db873f510bd19dac7
Aggelos Biboudis <biboudis@gmail.com>
2012-07-27 20:38:16 +0300

    Updated license. Added license.txt.

--- src/adapter/utils/GeospatialUtils.java
+++ src/adapter/utils/GeospatialUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Aggelos Biboudis.
+ * Copyright (c) 2012 IDIRA project.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
